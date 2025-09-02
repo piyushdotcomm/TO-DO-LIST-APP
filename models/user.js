ead5779de397377fb9214ce57b81b64cf007d405
Piyush Kumar <13725shpiyush@gmail.com>
2025-09-02 22:13:01 +0530

    Adding Password Encryption with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 const {schema} = mongoose;
-
+const bycrypt = required('bycrypt');
 const userSchema = new schema({
     firstNmae:String,
     lastName:String,
     username:{type:String,required:true},
     password:{type:String,required:true}
 });
+
+
+userSchema.pre("save",async function(next){
+    const user = this;
+    if(!user.isModified) return next();
+    let salt = await bycrypt.genSalt(10);
+    let hash = await bycrypt.hash(user.password,salt);
+    user.password = hash;
+    next();
+});
+
+
+
+userSchema.methods.comparePassword = async function (password){
+    return bycrypt.compare(password,this.password);
+}
+
+
 
 
 const User = mongoose.model("User",userSchema);
