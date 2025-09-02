e3b34f731e5ee4f17e0a12dd149543803ecc5443
Piyush Kumar <13725shpiyush@gmail.com>
2025-09-02 20:56:21 +0530

    Preparing the server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT,()=>(
+    console.log(`Server started at port ${PORT}`)
+))
