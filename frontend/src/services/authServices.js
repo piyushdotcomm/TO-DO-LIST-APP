b51236677b82fde6bde14f6e1952025d44a43a48
Piyush Kumar <13725shpiyush@gmail.com>
2025-09-03 12:34:49 +0530

    new folder

---
+++ frontend/src/services/authServices.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const SERVER_URL = 'https://to-do-list-app-hwfq.onrender.com/api';
+
+
+const registerUser = (data)=>{
+    return axios.post(SERVER_URL+'/register',data);
+}
+
+const loginUser = (data)=>{
+    return axios.post(SERVER_URL+'/login',data);
+}
+
+
+const AuthServices = {
+    registerUser,
+    loginUser
+}
+
+
+export default AuthServices;
